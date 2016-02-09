a00c831d86caffc24d42a4b2ab690af156983483
Daniel Andrus <danieleandrus@gmail.com>
2016-02-08 23:02:04 -0700

    Added NetBeans project and main class

---
+++ GUI-PA-1/src/com/hardcoders/csc468/weather/Weatherman.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hardcoders.csc468.weather;
+
+/**
+ *
+ * @author Johnathan Ackerman <johnathan.ackerman@mines.sdsmt.edu>
+ * @author Daniel Andrus <daniel.andrus@mines.sdsmt.edu>
+ * @author MacKenzie Smith <mackenzie.smith@mines.sdsmt.edu>
+ */
+public class Weatherman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
