def0a3b4b20c736855e430be011d60420b424d04
Johnathan Ackerman <johnny.ackerman@bowtaps.com>
2016-02-22 16:55:49 -0700

    added xml weatherpoint class

--- GUI-PA-1/src/com/hardcoders/csc468/weather/XMLImport.java
+++ GUI-PA-1/src/com/hardcoders/csc468/weather/XMLImport.java
@@ -1,6 +1,10 @@
 package com.hardcoders.csc468.weather;
+import com.hardcoders.csc468.weather.model.WeatherDataPoint;
+import com.hardcoders.csc468.weather.model.WindDirection;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -11,7 +15,10 @@ import org.jdom2.input.SAXBuilder;
  *
  * @author 7143145
  */
-public class XMLImport {
+public class XMLImport{
+    
+    static List<XmlWeatherDataPoint> yearOfPoints = new ArrayList<>();
+    
     public static void read( String fileName){
         SAXBuilder builder = new SAXBuilder();
         File currentXmlFile = new File(fileName);
@@ -20,16 +27,19 @@ public class XMLImport {
 
 		Document document = (Document) builder.build(currentXmlFile);
 		Element rootNode = document.getRootElement();
-		List list = rootNode.getChildren("staff");
-
+		List list = rootNode.getChildren("");
+                
 		for (int i = 0; i < list.size(); i++) {
 
 		   Element node = (Element) list.get(i);
+                   
+                   XmlWeatherDataPoint newPoint;
+                   //newPoint.time
 
-		   System.out.println("First Name : " + node.getChildText("firstname"));
-		   System.out.println("Last Name : " + node.getChildText("lastname"));
-		   System.out.println("Nick Name : " + node.getChildText("nickname"));
-		   System.out.println("Salary : " + node.getChildText("salary"));
+		   System.out.println("" + node.getChildText(""));
+		   System.out.println("" + node.getChildText(""));
+		   System.out.println("" + node.getChildText(""));
+		   System.out.println("" + node.getChildText(""));
 
 		}
 
@@ -38,6 +48,64 @@ public class XMLImport {
 	  } catch (JDOMException jdomex) {
 		System.out.println(jdomex.getMessage());
 	  }
+    }
+
+    class XmlWeatherDataPoint implements WeatherDataPoint
+    {
+        @Override
+        public Date getTimestamp() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getTemperature() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getHumidity() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getPressure() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getWindSpeed() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public WindDirection getWindDirection() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getWindGust() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getWindChill() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getHeatIndex() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getUVIndex() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Double getPercipitation() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
     }
     
 }
